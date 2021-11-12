a0073f2f7ceae52f943b80d0efae242cd16a9865
Thomas Geijtenbeek <tgeijten@gmail.com>
2021-11-12 12:37:05 +0100

    N*: Objective now supports auto_std_factor and auto_std_minimum

--- src/sconelib/scone/optimization/Objective.cpp
+++ src/sconelib/scone/optimization/Objective.cpp
@@ -18,8 +18,11 @@ namespace scone
 		if ( auto p = props.try_get_child( "Parameters" ) )
 		{
 			for ( auto& par : *p )
-				info().add( ParInfo( par.first, par.second ) );
+				info().add( ParInfo( par.first, par.second, info().options() ) );
 		}
+
+		props.try_get( info().options().auto_std_factor, "auto_std_factor" );
+		props.try_get( info().options().auto_std_minimum, "auto_std_minimum" );
 	}
 
 	Objective::~Objective()
