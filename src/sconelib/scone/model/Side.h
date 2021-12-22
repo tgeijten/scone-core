462ef1ed8459f047980b6b64fd99301d9cc827e5
Thomas Geijtenbeek <thomas@goatstream.com>
2021-12-22 13:26:57 +0100

    F: removed invalid and unused FindBySide()

--- src/sconelib/scone/model/Side.h
+++ src/sconelib/scone/model/Side.h
@@ -98,15 +98,6 @@ namespace scone
 	}
 
 	template< typename T >
-	T& FindBySide( std::vector< T >& cont, Side side )
-	{
-		using xo::to_str;
-		auto it = std::find_if( cont.begin(), cont.end(), [&]( T& item ) { return item->GetSide() == side; } );
-		SCONE_THROW_IF( it == cont.end(), "Could not find item with side " + to_str( side ) );
-		return *it;
-	}
-
-	template< typename T >
 	const T& FindByNameTrySided( const std::vector< T >& cont, const String& name, const Side& side )
 	{
 		using xo::quoted;
