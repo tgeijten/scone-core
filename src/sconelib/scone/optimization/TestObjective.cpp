c66e97c3f034dfb203ed9d2d873c122cade520bf
Thomas Geijtenbeek <thomas@goatstream.com>
2022-01-04 17:34:55 +0100

    N*: TestObjective now supports various test functions

--- src/sconelib/scone/optimization/TestObjective.cpp
+++ src/sconelib/scone/optimization/TestObjective.cpp
@@ -8,12 +8,28 @@
 
 #include "TestObjective.h"
 
-#include "spot_test/test_functions.h"
 #include "xo/string/string_tools.h"
 #include "scone/core/string_tools.h"
+#include "xo/numerical/math.h"
 
 namespace scone
 {
+	double sphere( const spot::par_vec& v )
+	{
+		double sum = 0.0;
+		for ( unsigned int i = 0; i < v.size(); ++i )
+			sum += xo::squared( v[ i ] );
+		return sum;
+	}
+
+	double rosenbrock( const spot::par_vec& v )
+	{
+		double sum = 0.0;
+		for ( unsigned int i = 0; i < v.size() - 1; i++ )
+			sum += 100 * xo::squared( v[ i + 1 ] - xo::squared( v[ i ] ) ) + xo::squared( 1. - v[ i ] );
+		return sum;
+	}
+
 	double schwefel( const spot::par_vec& v )
 	{
 		double sum = 0.0;
@@ -22,22 +38,59 @@ namespace scone
 		return 418.9829 * v.size() - sum;
 	}
 
-	TestObjective::TestObjective( const PropNode& pn, const path& find_file_folder ) :
-	Objective( pn, find_file_folder )
+	double rastrigin( const spot::par_vec& v )
 	{
-		INIT_PROP( pn, dim_, 10 );
+		double sum = 10.0 * v.size();
+		for ( index_t i = 0; i < v.size(); ++i )
+			sum += xo::squared( v[ i ] ) - 10.0 * cos( 2 * xo::constantsd::pi() * v[ i ] );
+		return sum;
+	}
 
+	auto get_function_ptr( TestObjectiveFunction fun )
+	{
+		switch ( fun )
+		{
+		case TestObjectiveFunction::Sphere: return &sphere;
+		case TestObjectiveFunction::Rosenbrock: return &rosenbrock;
+		case TestObjectiveFunction::Schwefel:return &schwefel;
+		case TestObjectiveFunction::Rastrigin: return &rastrigin;
+		default: xo_error( "Unsupported test function" );
+		}
+	}
+
+	xo::boundsd get_range( TestObjectiveFunction fun )
+	{
+		switch ( fun )
+		{
+		case TestObjectiveFunction::Sphere: return { -1e9, 1e9 };
+		case TestObjectiveFunction::Rosenbrock: return { -5, 10 };
+		case TestObjectiveFunction::Schwefel:return { -500, 500 };
+		case TestObjectiveFunction::Rastrigin: return { -5.12, 5.12 };
+		default: xo_error( "Unsupported test function" );
+		}
+	}
+
+	TestObjective::TestObjective( const PropNode& pn, const path& find_file_folder ) :
+		Objective( pn, find_file_folder ),
+		INIT_MEMBER( pn, function_, TestObjectiveFunction::Schwefel ),
+		INIT_MEMBER( pn, dim_, 10 ),
+		INIT_MEMBER( pn, range_, get_range( function_ ) ),
+		INIT_MEMBER( pn, mean_, range_.center() ),
+		INIT_MEMBER( pn, std_, range_.range() / 5 ),
+		fun_( get_function_ptr( function_ ) )
+	{
+		auto range = get_range( function_ );
 		for ( index_t i = 0; i < dim_; ++i )
-			info_.add( ParInfo( stringf( "P%d", i ), 0, 200, -500, 500 ) );
+			info_.add( ParInfo( stringf( "P%d", i ), mean_, std_, range_.lower, range_.upper ) );
 	}
 
 	fitness_t TestObjective::evaluate( const SearchPoint& point ) const
 	{
-		return schwefel( point.values() );
+		return fun_( point.values() );
 	}
 
 	String TestObjective::GetClassSignature() const
 	{
-		return stringf( "T%d", dim_ );
+		return to_str( function_ ) + to_str( dim_ );
 	}
 }
--- src/sconelib/scone/optimization/TestObjective.h
+++ src/sconelib/scone/optimization/TestObjective.h
@@ -9,23 +9,43 @@
 #pragma once
 
 #include "Objective.h"
+#include "xo/utility/smart_enum.h"
+#include "spot/spot_types.h"
+#include "scone/core/Range.h"
+#include "xo/numerical/bounds.h"
 
 namespace scone
 {
-	/// Objective used for testing, evaluates a multi-dimensional Schwefel function.
+	/// TestObjective function types
+	xo_smart_enum_class( TestObjectiveFunction, Sphere, Rosenbrock, Schwefel, Rastrigin );
+
+	/// Objective used for testing
 	class SCONE_API TestObjective : public Objective
 	{
 	public:
 		TestObjective( const PropNode& pn, const path& find_file_folder );
 		virtual ~TestObjective() {}
 
+		/// Type of the function; default Schwefel
+		TestObjectiveFunction function_;
+
 		/// Dimension of the objective function
 		size_t dim_;
+
+		/// Parameter value range
+		xo::boundsd range_;
+
+		/// Value mean
+		Real mean_;
+
+		/// Value std
+		Real std_;
 
 		virtual fitness_t evaluate( const SearchPoint& point ) const override;
 
 	protected:
 		virtual String GetClassSignature() const override;
 
+		double ( *fun_ )( const spot::par_vec& );
 	};
 }
