db7ee2e9500d512dbc89987d230547bab7dd733c
Thomas Geijtenbeek <thomas@goatstream.com>
2022-01-28 13:44:01 +0100

    U: SpinalController updates (work in progress)

--- src/sconelib/scone/controllers/SpinalController.cpp
+++ src/sconelib/scone/controllers/SpinalController.cpp
@@ -2,8 +2,23 @@
 
 namespace scone
 {
+	MuscleGroup::MuscleGroup( const PropNode& pn, SpinalController& sc ) :
+		INIT_MEMBER_REQUIRED( pn, name_ ),
+		INIT_MEMBER( pn, antagonists_, {} ),
+		INIT_MEMBER( pn, synergists_, {} ),
+		INIT_MEMBER( pn, linked_, {} )
+	{}
+
+	NeuronGroup::NeuronGroup( const PropNode& pn, SpinalController& sc ) :
+		INIT_MEMBER_REQUIRED( pn, type_ )
+	{}
+
+	NeuronLink::NeuronLink( const PropNode& pn, SpinalController& sc ) :
+		INIT_MEMBER_REQUIRED( pn, type_ ),
+		INIT_MEMBER_REQUIRED( pn, activation_ )
+	{}
+
 	SpinalController::SpinalController( const PropNode& pn, Params& par, Model& model, const Location& loc ) :
 		Controller( pn, par, model, loc )
-	{
-	}
+	{}
 }
--- src/sconelib/scone/controllers/SpinalController.h
+++ src/sconelib/scone/controllers/SpinalController.h
@@ -1,12 +1,37 @@
 #pragma once
 
 #include "Controller.h"
+#include "xo/utility/smart_enum.h"
+#include "scone/optimization/Params.h"
 
 namespace scone
 {
+	class SpinalController;
+
 	struct MuscleGroup {
+		MuscleGroup( const PropNode& pn, SpinalController& sc );
 		string name_;
 		std::vector<Muscle*> muscles_;
+		std::vector<string> antagonists_;
+		std::vector<string> synergists_;
+		std::vector<string> linked_;
+	};
+
+	xo_smart_enum_class( NeuronGroupType, spindle, force, vestibular, motor, group );
+	struct NeuronGroup {
+		NeuronGroup( const PropNode& pn, SpinalController& sc );
+		String name_;
+		NeuronGroupType type_;
+	};
+
+	xo_smart_enum_class( NeuronLinkType, identical, antagonists, synergists );
+	xo_smart_enum_class( ActivationType, relu, leaky_relu, tanh, tanh_norm );
+	struct NeuronLink {
+		NeuronLink( const PropNode& pn, SpinalController& sc );
+		string input_;
+		string output_;
+		NeuronGroupType type_;
+		ActivationType activation_;
 	};
 
 	class SpinalController : public Controller
