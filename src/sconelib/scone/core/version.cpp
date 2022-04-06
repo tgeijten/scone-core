31ab6a8ed24a6db6084037c3ba1b72b9fbb74c3c
Thomas Geijtenbeek <thomas@goatstream.com>
2022-04-06 12:43:21 +0200

    V: 2.0.2

--- src/sconelib/scone/core/version.cpp
+++ src/sconelib/scone/core/version.cpp
@@ -15,7 +15,7 @@
 
 constexpr int SCONE_VERSION_MAJOR = 2;
 constexpr int SCONE_VERSION_MINOR = 0;
-constexpr int SCONE_VERSION_PATCH = 1;
+constexpr int SCONE_VERSION_PATCH = 2;
 constexpr const char* SCONE_VERSION_POSTFIX = "";
 
 namespace scone
