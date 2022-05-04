32ae8a8218dfe9dc75acaac08eb40808b9e0e6cf
Thomas Geijtenbeek <tgeijten@gmail.com>
2022-05-04 13:51:28 +0200

    V: 2.1.0 BETA 1

--- src/sconelib/scone/core/version.cpp
+++ src/sconelib/scone/core/version.cpp
@@ -14,9 +14,9 @@
 #include <fstream>
 
 constexpr int SCONE_VERSION_MAJOR = 2;
-constexpr int SCONE_VERSION_MINOR = 0;
-constexpr int SCONE_VERSION_PATCH = 3;
-constexpr const char* SCONE_VERSION_POSTFIX = "";
+constexpr int SCONE_VERSION_MINOR = 1;
+constexpr int SCONE_VERSION_PATCH = 0;
+constexpr const char* SCONE_VERSION_POSTFIX = "BETA 1";
 
 namespace scone
 {
