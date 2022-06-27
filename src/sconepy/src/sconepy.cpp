153ae157e3ae50cf5e23265a01db2f68a04b2afa
Thomas Geijtenbeek <thomas@goatstream.com>
2022-06-27 17:17:37 +0200

    N: sconepy Model and Muscle updates

--- src/sconepy/src/sconepy.cpp
+++ src/sconepy/src/sconepy.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include "scone/core/version.h"
 #include "xo/string/string_cast.h"
@@ -7,6 +8,9 @@
 #include "xo/filesystem/path.h"
 #include "scone/optimization/opt_tools.h"
 #include "scone/sconelib_config.h"
+#include "scone/model/Model.h"
+#include "spot/par_io.h"
+#include "scone/model/Muscle.h"
 
 namespace py = pybind11;
 
@@ -18,13 +22,60 @@ void evaluate_par_file( const std::string& file ) {
 	auto results = scone::EvaluateScenario( scenario_pn, file, file );
 	scone::log::info( results );
 }
+scone::ModelUP load_model( const std::string& file ) {
+	auto file_path = xo::path( file );
+	auto model_pn = load_zml( file_path );
+	auto model_props = scone::FindFactoryProps( scone::GetModelFactory(), model_pn, "Model" );
+	return scone::CreateModel( model_props, spot::null_objective_info(), file_path.parent_path() );
+}
+std::map< std::string, double > get_state( const scone::Model& m ) {
+	std::map< std::string, double > dict;
+	auto& s = m.GetState();
+	for ( size_t i = 0; i < s.GetSize(); ++i )
+		dict[ s.GetName( i ) ] = s.GetValue( i );
+	return dict;
+};
+void set_state( scone::Model& m, const std::map< std::string, double >& dict ) {
+	scone::State s = m.GetState();
+	for ( auto& [key, value] : dict )
+		s.SetValue( key, value );
+	m.SetState( s, 0.0 );
+};
+scone::Muscle& get_muscle( const scone::Model& m, size_t i ) { return *m.GetMuscles()[ i ]; }
 
-PYBIND11_MODULE(sconepy, m) {
+PYBIND11_MODULE( sconepy, m ) {
 	static xo::log::console_sink console_sink( xo::log::level::trace );
 	scone::Initialize();
 
-    m.attr("__version__") = xo::to_str( scone::GetSconeVersion() );
-
+	m.def( "scone_version", []() { return xo::to_str( scone::GetSconeVersion() ); } );
 	m.def( "data_dir", []() { return scone::GetDataFolder().str(); } );
-    m.def( "evaluate_par_file", &evaluate_par_file );
+	m.def( "set_log_level", []( int l ) { console_sink.set_log_level( xo::log::level( l ) ); } );
+	m.def( "evaluate_par_file", &evaluate_par_file );
+	m.def( "load_model", &load_model );
+
+	py::class_<scone::Muscle>( m, "Muscle" )
+		.def( "get_name", &scone::Muscle::GetName )
+		.def( "get_activation", &scone::Muscle::GetActivation )
+		.def( "init_activation", &scone::Muscle::InitializeActivation )
+		.def( "get_excitation", &scone::Muscle::GetExcitation )
+		.def( "set_excitation", &scone::Muscle::SetExcitation )
+		.def( "get_force", &scone::Muscle::GetForce )
+		.def( "get_normalized_force", &scone::Muscle::GetNormalizedForce )
+		.def( "get_fiber_length", &scone::Muscle::GetFiberLength )
+		.def( "get_normalized_fiber_length", &scone::Muscle::GetNormalizedFiberLength )
+		.def( "get_fiber_velocity", &scone::Muscle::GetFiberVelocity )
+		.def( "get_normalized_fiber_velocity", &scone::Muscle::GetNormalizedFiberVelocity )
+		;
+
+	py::class_<scone::Model>( m, "Model" )
+		.def( "get_name", &scone::Model::GetName )
+		.def( "advance_simulation_to", &scone::Model::AdvanceSimulationTo )
+		.def( "get_state", &get_state )
+		.def( "set_state", &set_state )
+		.def( "muscle_count", []( const scone::Model& m ) { return m.GetMuscles().size(); } )
+		.def( "get_muscle", &get_muscle, py::return_value_policy::reference )
+		.def( "get_time", &scone::Model::GetTime )
+		.def( "set_store_data", &scone::Model::SetStoreData )
+		.def( "write_results", []( const scone::Model& m, const std::string& f ) { m.WriteResults( f ); } )
+		;
 }
