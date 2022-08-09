f9dec3289328f96ac1ace717484980e21b1de1bb
Thomas Geijtenbeek <thomas@goatstream.com>
2022-08-09 17:24:44 +0200

    U: model_test

--- src/sconeunittests/model_test.cpp
+++ src/sconeunittests/model_test.cpp
@@ -9,7 +9,7 @@
 
 namespace scone
 {
-	std::vector<State> record_states( Model& m, TimeInSeconds end_time = 0.05 ) {
+	std::vector<State> record_states( Model& m, TimeInSeconds end_time = 1.0 ) {
 		std::vector<State> s;
 		for ( TimeInSeconds t = 0.0; t < end_time; t += 0.01 ) {
 			m.AdvanceSimulationTo( t );
@@ -18,36 +18,39 @@ namespace scone
 		return s;
 	}
 
-	XO_TEST_CASE_SKIP( model_test )
+	XO_TEST_CASE( model_test )
 	{
 		auto modeldir = GetFolder( SCONE_ROOT_FOLDER ) / "resources/unittestdata/models";
 
-		auto model_pn = xo::load_zml( modeldir / "H0918_hyfydy.scone" );
-		//auto model_pn = xo::load_zml( modeldir / "H1622_hyfydy.scone" );
+		//auto model_pn = xo::load_zml( modeldir / "H0918_hyfydy.scone" );
+		auto model_pn = xo::load_zml( modeldir / "H1622_hyfydy.scone" );
 		auto model_props = FindFactoryProps( GetModelFactory(), model_pn, "Model" );
 
 		spot::null_objective_info par;
 		auto model = CreateModel( model_props, par, modeldir );
-		const TimeInSeconds end_time = 0.4;
-		State sa0 = model->GetState();
-		model->AdvanceSimulationTo( end_time );
-		State sa1 = model->GetState();
-		model->ResetState();
-		State sb0 = model->GetState();
-		model->AdvanceSimulationTo( end_time );
-		State sb1 = model->GetState();
-		XO_CHECK( sa0 == sb0 );
-		XO_CHECK( sa1 == sb1 );
 
-		//std::vector<State> sa, sb;
-		//sa = record_states( *model );
-		//model->ResetState();
-		//sb = record_states( *model );
-		//model->ResetState();
-		//sc = record_states( *model );
-		//for ( index_t i = 0; i < sa.size(); ++i ) {
-		//	log::info( i, ": ", sa[ i ] == sb[ i ] );
-		//}
-		//XO_CHECK( sa == sb );
+		double duration = 1.0;
+
+		State s0_a = model->GetState();
+		model->AdvanceSimulationTo( duration );
+		auto step_a = model->GetIntegrationStep();
+		State s1_a = model->GetState();
+
+		model->ResetState();
+		State s0_b = model->GetState();
+		model->AdvanceSimulationTo( duration );
+		auto step_b = model->GetIntegrationStep();
+		State s1_b = model->GetState();
+
+		XO_CHECK( step_a == step_b );
+		XO_CHECK( s1_a == s1_b );
+		XO_CHECK( s0_a == s0_b );
+
+		std::vector<State> sa, sb;
+		model->ResetState();
+		sa = record_states( *model );
+		model->ResetState();
+		sb = record_states( *model );
+		XO_CHECK( sa == sb );
 	}
 }
