b884b2eba913dcc1f836466326a80de01753fb2f
Thomas Geijtenbeek <thomas@goatstream.com>
2022-08-11 20:54:37 +0200

    V: 2.1.0-BETA-4

--- src/sconelib/scone/core/version.cpp
+++ src/sconelib/scone/core/version.cpp
@@ -18,7 +18,7 @@
 	constexpr int SCONE_VERSION_MAJOR = 2;
 	constexpr int SCONE_VERSION_MINOR = 1;
 	constexpr int SCONE_VERSION_PATCH = 0;
-	constexpr const char* SCONE_VERSION_POSTFIX = "BETA 3";
+	constexpr const char* SCONE_VERSION_POSTFIX = "BETA 4";
 #else
 	constexpr int SCONE_VERSION_MAJOR = 2;
 	constexpr int SCONE_VERSION_MINOR = 0;
