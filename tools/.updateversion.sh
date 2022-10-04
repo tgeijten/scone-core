2013ed1e78d9d0e21b08124226449614c44e29a1
Thomas Geijtenbeek <thomas@goatstream.com>
2022-10-04 16:41:26 +0200

    B: CPack installer name updates

--- tools/.updateversion.sh
+++ tools/.updateversion.sh
@@ -1,1 +1,8 @@
-git rev-list --count HEAD > "../.version"
+#!/bin/sh
+
+# Create a file repository_revision.h that defines a macro REPOSITORY_REVISION
+echo -n '#define SCONE_REPOSITORY_REVISION ' > "./src/repository_revision.h"
+git rev-list --count HEAD >> "./src/repository_revision.h"
+
+# Create a file .version that contains the current commit count
+git rev-list --count HEAD > "./.version"
