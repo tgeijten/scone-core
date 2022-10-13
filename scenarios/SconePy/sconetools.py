ab5ba4e40e5a61f13f727c0cd6984e4043f35ea6
Thomas Geijtenbeek <thomas@goatstream.com>
2022-10-13 10:18:02 +0200

    U: sconetools version check

--- scenarios/SconePy/sconetools.py
+++ scenarios/SconePy/sconetools.py
@@ -1,6 +1,9 @@
 import sys
-import glob
+import platform
 import pathlib
+
+if sys.version_info < (3,9) or sys.version_info >= (3,10):
+    raise Exception('SconePy only supports Python 3.9 -- current version: ' + platform.python_version() )
 
 path_to_sconepy = ''
 
@@ -11,21 +14,17 @@ def try_find_sconepy(path):
     if sorted(pathlib.Path(path).glob('sconepy*.*')):
         path_to_sconepy = str(path)
 
-try:
-    # check if sconepy is already in path
-    import sconepy
-except ModuleNotFoundError:
-    if sys.platform.startswith('win'):
-        try_find_sconepy('C:/Program Files/SCONE/bin')
-        try_find_sconepy('D:/Build/scone-studio/vc2019-x64/bin/Release')
-    elif sys.platform.startswith('linux'):
-        try_find_sconepy('/opt/scone-core/lib')
-        try_find_sconepy(pathlib.Path.home() / 'scone-core/lib')
+if sys.platform.startswith('win'):
+    try_find_sconepy('C:/Program Files/SCONE/bin')
+    try_find_sconepy('D:/Build/scone-studio/vc2019-x64/bin/Release')
+elif sys.platform.startswith('linux'):
+    try_find_sconepy('/opt/scone-core/lib')
+    try_find_sconepy(pathlib.Path.home() / 'scone-core/lib')
 
-    if path_to_sconepy:
-        print('SconePy found at', path_to_sconepy)
-        sys.path.append(path_to_sconepy)
-    else:
-        raise Exception('Could not find SconePy')
+if path_to_sconepy:
+    print('SconePy found at', path_to_sconepy)
+    sys.path.append(path_to_sconepy)
+else:
+    raise Exception('Could not find SconePy')
 
-    import sconepy
+import sconepy
