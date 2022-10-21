9e20a45856929db7ad2fce974eee0606642d7207
Thomas Geijtenbeek <thomas@goatstream.com>
2022-10-21 15:09:00 +0200

    B: linux_create-scone-core/studio-package.sh

---
+++ tools/linux_build-scone-core-depencies.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+export CMAKE_VERBOSE_MAKEFILE="${CMAKE_VERBOSE_MAKEFILE:-False}"
+
+source "${script_dir}/build_config"
+
+${script_dir}/linux_1_get-external-dependencies
+${script_dir}/unix_2b_build-simbody-fix
+${script_dir}/unix_2c_build-opensim3-fix
--- tools/linux_create-scone-core-package.sh
+++ tools/linux_create-scone-core-package.sh
@@ -10,7 +10,7 @@ source "${script_dir}/build_config"
 # ${script_dir}/linux_1_get-external-dependencies
 # ${script_dir}/unix_2b_build-simbody-fix
 # ${script_dir}/unix_2c_build-opensim3-fix
-${script_dir}/unix_2d_build-scone-hfd
+${script_dir}/unix_2d_build-scone-core-hfd
 
 # install and package
 cd "${SCONE_BUILD_DIR}"
