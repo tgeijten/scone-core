b9a49e2ea1396c94b8fcce5b0515fb1742911521
Thomas Geijtenbeek <thomas@goatstream.com>
2023-01-04 14:14:53 +0100

    V: 2.1.0-RC-2

--- src/sconelib/scone/core/version.cpp
+++ src/sconelib/scone/core/version.cpp
@@ -18,12 +18,12 @@
 	constexpr int SCONE_VERSION_MAJOR = 2;
 	constexpr int SCONE_VERSION_MINOR = 1;
 	constexpr int SCONE_VERSION_PATCH = 0;
-	constexpr const char* SCONE_VERSION_POSTFIX = "RC 1";
+	constexpr const char* SCONE_VERSION_POSTFIX = "RC 2";
 #else
 	constexpr int SCONE_VERSION_MAJOR = 2;
 	constexpr int SCONE_VERSION_MINOR = 1;
 	constexpr int SCONE_VERSION_PATCH = 0;
-	constexpr const char* SCONE_VERSION_POSTFIX = "RC 1";
+	constexpr const char* SCONE_VERSION_POSTFIX = "RC 2";
 #endif
 
 #if __has_include( "scone/../../repository_revision.h" )
