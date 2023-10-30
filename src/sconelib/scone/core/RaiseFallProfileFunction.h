32428a1fa1d5f371baf4cde36ba90238d5eb9b5d
Thomas Geijtenbeek <thomas@goatstream.com>
2023-10-30 17:48:51 +0100

    D: RaiseFallProfileFunction doc

--- src/sconelib/scone/core/RaiseFallProfileFunction.h
+++ src/sconelib/scone/core/RaiseFallProfileFunction.h
@@ -13,15 +13,25 @@
 namespace scone
 {
 	/// Parameterized raise-fall function. 
+	/// Implementation of the two joined sine curve for raise and fall profile function. 
+	/// For more details, see: Ding, Walsh et al., Human-in-the-loop optimization of hip assistance with a soft exosuit during walking,
+	/// Science Robotics, 2018.
 	class SCONE_API RaiseFallProfileFunction : public Function
 	{
 	public:
 		RaiseFallProfileFunction( const PropNode& props, Params& par );
 		virtual ~RaiseFallProfileFunction();
 
+		/// Profile start time [s]; default = not set
 		const PropNode& start_time;
+
+		/// Profile raise time [s]; default = not set
 		const PropNode& raise_time;
+
+		/// Profile fall time [s]; default = not set
 		const PropNode& fall_time;
+
+		/// Profile peak; default = not set
 		const PropNode& peak;
 
 		virtual Real GetValue( Real x ) override;
