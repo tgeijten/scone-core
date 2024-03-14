d0582f04dce55564c93b570d2f60ccebff26ee8f
Thomas Geijtenbeek <thomas@goatstream.com>
2024-03-14 16:11:47 +0100

    U: DisplayGeometry now had GeometryType

--- src/sconelib/scone/model/DisplayGeometry.h
+++ src/sconelib/scone/model/DisplayGeometry.h
@@ -9,23 +9,24 @@
 
 namespace scone
 {
+	enum class DisplayGeometryType { file_mesh, shape, aux_shape };
 	struct DisplayGeometry
 	{
 		DisplayGeometry( const xo::path& file, const Vec3& p, const Quat& q = Quat(), const Vec3& s = Vec3::one(), const xo::color& c = xo::color::null() ) :
-			filename_( file ),
-			shape_(),
-			pos_( p ),
-			ori_( q ),
-			scale_( s ),
-			color_( c )
+			filename_( file ), shape_(),
+			pos_( p ), ori_( q ), scale_( s ), color_( c ),
+			type_( DisplayGeometryType::file_mesh )
+
 		{}
 		DisplayGeometry( const xo::shape& sh, const Vec3& p, const Quat& q = Quat(), const Vec3& s = Vec3::one(), const xo::color& c = xo::color::null() ) :
-			filename_(),
-			shape_( sh ),
-			pos_( p ),
-			ori_( q ),
-			scale_( s ),
-			color_( c )
+			filename_(), shape_( sh ),
+			pos_( p ), ori_( q ), scale_( s ), color_( c ),
+			type_( DisplayGeometryType::shape )
+		{}
+		DisplayGeometry( const xo::shape& sh, const Vec3& p, const Quat& q, const Vec3& s, DisplayGeometryType t ) :
+			filename_(), shape_( sh ),
+			pos_( p ), ori_( q ), scale_( s ), color_( xo::color::null() ),
+			type_( t )
 		{}
 
 		xo::path filename_;
@@ -34,5 +35,6 @@ namespace scone
 		Quat ori_;
 		Vec3 scale_;
 		xo::color color_;
+		DisplayGeometryType type_;
 	};
 }
