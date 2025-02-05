61d57edcee35996899dbd3a5bbf7bae598e4b674
Thomas Geijtenbeek <thomas@goatstream.com>
2025-02-05 17:52:23 +0100

    D: generate.bat, sconepy_help.txt

--- doc/generate.bat
+++ doc/generate.bat
@@ -1,7 +1,7 @@
 del xml\*.* /Q
 del ref\*.* /Q
 "C:\Program Files\doxygen\bin\doxygen.exe" Doxyfile
-"..\..\..\dokugen\bin\vc2019-x64\Release\dokugen.exe" xml ref -r classscone_1_1_ -r structscone_1_1_
+"..\..\..\dokugen\bin\vc2019-x64\Release\dokugen.exe" xml ref -r classscone_1_1_ -r structscone_1_1_ -r s_c_o_n_e_h_f_d___n_a_m_e_s_p_a_c_e_1_1_
 copy ref\*_hfd.txt ref\*_hyfydy.txt
 dir /b ref\*.txt > "..\..\resources\help\keywords.txt"
 
